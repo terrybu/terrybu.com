bde4c651b1af3723ee1eb6023e173be88c1ee95c
Terry Bu <tbu@payfone.com>
2018-11-14 18:01:11 -0500

    changes to htaccess file to force https

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@
 
 <div id="bodyContent">
 <h1>Contact Terry</h1>
-  <p align='center'>Please email me at <a href="mailto:JupiterMillennial@gmail.com" target="_blank">JupiterMillennial@gmail.com</a>
+  <p align='center'>Please email me at <a href="mailto:terrybu88@gmail.com" target="_blank">terrybu88@gmail.com</a>
   </p>
 <!-- <div id="large">
 
