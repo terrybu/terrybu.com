4d48d4db8f63529ad233bee8a07dad17d56527fd
Terry Bu <terrybu88@gmail.com>
2019-09-20 11:39:48 -0400

    Added Book Writing as a navigation panel and page. Fixed naming conventions

---
+++ book_writing.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include ("_includes/standardHeadImports.php"); ?>
+  <title>Terry Bu - Personal Website | Book Writing </title>
+</head>
+<body>
+    <div class="container">
+        <div id="browser"><p>*Optimal in 1920x1080 resolution/Chrome</p></div>
+        <?php include("_includes/gotSocial.php"); ?>
+        <?php include("_includes/welcomeNav.php"); ?>
+        <?php include("_includes/mainLandingLeftImage.php"); ?>
+
+        <div id="bodyContent">
+          <h1 style="color: #C63B3B; clear: both;">Authored Books</h1>
+          <ul>
+               <h2 style="color: black">당찬 가족의 성공유학 프로젝트 (2005)</h2>
+               <p>Translation: “Successful Study Abroad Project of a Courageous Family"</p>
+               <p>A family autobiography and a self-help memoir about a Korean-American family and their journey and adventures facing and overcoming cultural obstacles in a foreign country. Tips and advice on how to adjust to an American high school, learn English as a second language and fit into a new community. Primary readership audience were Korean students and parents contemplating immigration to a foreign country.</p>
+                <p><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=573396" target="blank">Korean Book Site Link</a></p>
+                <img src="_images/book_writing/dangchan_cover.jpg" style="max-height: 500px; max-width: 500px;">
+                <img src="_images/book_writing/dangchan_sample.jpg" style="max-height: 500px; max-width: 500px;">
+                <img src="_images/book_writing/dangchan_familypic.jpg" style="max-height: 300px; max-width: 300px;">
+
+                <br>
+                <br>
+                <br>
+
+                <h2 style="color: black">Diaries of My Older Sister: Depression and Suicide in Korea, Asia and America (2019)</h2>
+                <p>A non-fiction memoir and a self-help book dedicated to the author's older sister. This book goes into the types of thought patterns and cognitive distortions that might cause obsessive, harmful overthinking known as rumination which has been proven to be a major cause of major depression. It also discusses possible solutions at both individual and community levels, and why we need to address societal issues such as our definition of success, especially in South Korean and Asian-American communities.</p>
+                <p>Projected Release Date: December 2019 on Amazon Kindle and Apple iBooks</p>
+                <img src="_images/book_writing/diaries_cover.jpg" style="max-height: 500px; max-width: 500px;">
+          </ul>
+        </div>
+    </div>
+
+
+
+<!--universal includes-->
+<?php include("_includes/viewSizeDisplay.php"); ?>
+<!--universal javascript-->
+<script type="text/javascript" src="_javascript/nav.js"></script>
+<!--universal sound effect -->
+<audio id="1up" src="_sound/swim.wav"></audio>
+<div id="sounddiv"><bgsound id="sound"></div>
+
+</body>
+</html>
--- personal_photos.php
+++ personal_photos.php
@@ -8,7 +8,7 @@
    <script type="text/javascript" src="https://s3.amazonaws.com/codecademy-content/courses/hour-of-code/js/alphabet.js"></script>
    <link href="_css/personal_projects.css" rel="stylesheet" type="text/css">
    <link rel="stylesheet" type="text/css" href="_css/colorbox.css">
-   <title>Terry Bu - Personal Website | Personal Photos</title>
+   <title>Terry Bu - Personal Website | Photos</title>
 </head>
 <body>
 
@@ -20,7 +20,7 @@
         <?php include("_includes/mainLandingLeftImage.php"); ?>
 
         <div id="bodyContent">
-          <h1 style="color: #C63B3B; clear: both;">Personal Photos</h1>
+          <h1 style="color: #C63B3B; clear: both;">Photos</h1>
           <ul>
             <li>
                 <p>2009: UNC DSP International Business Fraternity</p>
