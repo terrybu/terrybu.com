49c2796d24a3b3da698da00d7fbfc7ff587049e5
Terry Bu <tbu@payfone.com>
2020-02-07 16:36:37 -0500

    placement testing

--- index.php
+++ index.php
@@ -87,16 +87,17 @@ require('blog/wp-blog-header.php');
 </div>
 </div>
 
-<?php
-$posts = get_posts('numberposts=10&order=ASC&orderby=post_title');
-foreach ($posts as $post) : setup_postdata( $post ); ?>
-  <?php the_date(); echo "<br />"; ?>
-  <?php the_title(); ?>    
-  <?php the_excerpt(); ?> 
+<div style="margin-top: 16em;">
   <?php
-endforeach;
-?>
-
+  $posts = get_posts('numberposts=10&order=ASC&orderby=post_title');
+  foreach ($posts as $post) : setup_postdata( $post ); ?>
+    <?php the_date(); echo "<br />"; ?>
+    <?php the_title(); ?>    
+    <?php the_excerpt(); ?> 
+    <?php
+  endforeach;
+  ?>
+</div>
 
 </div> <!-- /bodyContent -->  
 </div> <!-- /container -->  
