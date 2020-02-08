494b850759a857f6c803681536566f0770c27379
Terry Bu <terrybu88@gmail.com>
2020-02-08 15:28:50 -0500

    Remove blogspot

--- _includes/gotSocial.php
+++ _includes/gotSocial.php
@@ -2,13 +2,11 @@
 <span>got Social?</span>
 <a href="https://www.instagram.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_instagram.png" alt="terry instagram"></a>
 
-<a href="https://www.twitter.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_twitter.png" alt="terry twitter"></a>
-
 <a href="https://www.youtube.com/burownrice" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_youtube.png" alt="terry youtube"></a>
 
-<a href="https://terrybu88.blogspot.com" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_blogger.png" alt="terry's blog on blogspot.com"></a>
-
 <a href="http://www.quora.com/Terry-Bu" target="_blank"><img src="_images/icon_quora.png" alt="terry quora" onmouseover="document.getElementById('1up').play()"></a>
+
+<a href="https://www.twitter.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_twitter.png" alt="terry twitter"></a>
 
 <a href="https://www.linkedin.com/in/terrybu" target="_blank"><img src="_images/icon_linkedin_new.png" alt="terry linkedin" onmouseover="document.getElementById('1up').play()"></a>
 
