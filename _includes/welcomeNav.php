d0d7546d866eb238339880537435ea830ac25ef2
Terry Bu <tbu@payfone.com>
2021-11-29 10:26:33 -0700

    Kor text change

--- _includes/welcomeNav.php
+++ _includes/welcomeNav.php
@@ -5,7 +5,7 @@
 	<div id="welcome">
 	  <p><b>Terry Bu 부성현</b></p>
 	  <p>Thank you for visiting my website.</p>
-	  <p>방문해주시는 여러분들에게 감사드립니다 🙇</p>
+	  <p>방문해주셔서 감사드립니다. 🙇</p>
 	</div>
 
 	<div id="nav">
