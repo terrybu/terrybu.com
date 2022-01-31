8ebbb0b69ba49a5f64fa371cb8d32d5e9b23b18d
Terry Bu <tbu@payfone.com>
2022-01-31 09:45:33 -0700

    scss change

--- _includes/mainLandingLeftImage.php
+++ _includes/mainLandingLeftImage.php
@@ -1,4 +1,4 @@
 	<div id="mainLandingLeftImage">
-		<img src="_images/profile_pic_recent2.jpg" style="border:0" width="500" height="789">
+		<img src="_images/profile_pic_recent2.jpg" style="border:0">
 	</div>
 </div>
