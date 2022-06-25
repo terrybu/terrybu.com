53b0f44dbc77b10f39a60625b401e04e38d81fb4
Terry Bu <tbu@payfone.com>
2022-06-25 11:35:26 -0600

    Adding spotify podcast link

--- _includes/gotSocial.php
+++ _includes/gotSocial.php
@@ -2,16 +2,18 @@
 <span>got Social?</span>
 <a href="https://www.instagram.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_instagram.png" alt="terry instagram"></a>
 
-<a href="https://github.com/terrybu" target="_blank"><img src="_images/github.png" alt="terry github" onmouseover="document.getElementById('1up').play()" width="45" height="45"></a>
 <!-- 
 <a href="https://stackoverflow.com/story/terrybu" target="_blank"><img src="_images/icon_stack.png" alt="terry stackoverflow" onmouseover="document.getElementById('1up').play()" width="45" height="45"></a> -->
 
-<a href="https://www.youtube.com/burownrice" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_youtube.png" alt="terry youtube"></a>
+<a href="https://www.youtube.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_youtube.png" alt="terry youtube"></a>
+<a href="https://open.spotify.com/show/0z5lX7csyyWssIw8AQDqAz?si=8316c6c2b4ae44da" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_spotify.png" alt="terry podcast"></a>
+
 
 <!-- <a href="http://www.quora.com/Terry-Bu" target="_blank"><img src="_images/icon_quora.png" alt="terry quora" onmouseover="document.getElementById('1up').play()"></a>
 
 <a href="https://www.twitter.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_twitter.png" alt="terry twitter"></a>-->
 
-<a href="https://www.linkedin.com/in/terrybu" target="_blank"><img src="_images/icon_linkedin_new.png" alt="terry linkedin" onmouseover="document.getElementById('1up').play()"></a> 
+<a href="https://www.linkedin.com/in/terrybu" target="_blank"><img src="_images/icon_linkedin_new.png" alt="terry linkedin" onmouseover="document.getElementById('1up').play()"></a>
+<a href="https://github.com/terrybu" target="_blank"><img src="_images/github.png" alt="terry github" onmouseover="document.getElementById('1up').play()" width="45" height="45"></a>
 
 </div>
