2c44e9383b00b1dd99dc388435b95003909d40bf
Terry Bu <terry.bu@RELEX-Macbook-Pro-Terry.local>
2022-12-13 09:14:06 -0700

    Added original korean draft

--- books.php
+++ books.php
@@ -61,8 +61,8 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
 
                 <!-- <a href="https://www.terrybu.com/diaries-kor" target="blank"> korean-sangshil-v1.0.pdf  -->
-
-                <h1><a href="https://www.terrybu.com/downloads/korean-sangshil-v1.0.pdf" target="blank"> 무료 PDF 다운로드 --> "상실을 넘어서다" (2022)</a></h1>
+		<h1><a href="https://terrybu.com/downloads/diaries-korean-water.pdf"> 한국어 번역판 원본 "누나의 일기: 세계의 청년들에게 일어나는 우울증과 자살 " (2022)</a></h1>
+                <h1><a href="https://www.terrybu.com/downloads/korean-sangshil-v1.0.pdf" target="blank"> 무료 PDF --> "상실을 넘어서다" (2022) (한국에서 출판가능성이 있었던 원고이지만 제가 개인적으로 작업한 위의 번역판과는 약간 내용이 다릅니다. 둘다 참고해주세요)</a></h1>
 
                 <p>지난 5-10년부터 2021년이란 시간동안 한국의 자살률은 세계 최고 수준입니다. OECD 국가 간 자살률을 비교 시 OECD 평균 10.9명에 비해, 한국은 23.5명(2020년 기준)으로 가장 높은 수준을 기록했다고 합니다. 한국의 자살은 10대부터 30대까지 사망원인 순위 1위이고, 40대, 50대에서는 사망원인 순위 2위를 차지했습니다. 저의 친누나는 2005년 심한 우울증을 겪은 뒤 20살의 나이에 대학교 기숙사에서 자결하였고 저는 지난 15년간 누나에게 왜 그런 일이 있어야만 했는지를 알아내기 위해서 우울증의 진리에 대해서 탐구했습니다. 이 책안에서 누나의 개인적인 사정과 정신세계뿐만 아니라 우울증에 취약하신 분들에게 쉽게 일어나는 부정적인 반추 (negative rumination) 와 반복적인 부정적인 사고에 대해서 분석했고 우울증이 일어나는 복합적인 원인, 그리고 장래의 해결책으로서 사회적으로 그리고 개인적으로 할 수 있는 습관들이 무엇인지를 추천해보았습니다. 이 책을 장래의 한국의 청년들을 위해서 바칩니다. </p>
 
