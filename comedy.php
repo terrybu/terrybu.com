98914c9b1d95a9220f51e77e1db9ffd14980f241
Terry Bu <terrybu88@gmail.com>
2023-11-10 10:41:18 -0800

    changes comedy

--- comedy.php
+++ comedy.php
@@ -41,26 +41,23 @@
 
 			<p><h2>2013-2016</h2></p>
 
-			<p><h4>Greenwich Village Comedy Club Showcase with Talia Reese</p></h4>
-			<ui><iframe src="https://www.youtube.com/embed/9g_1bIP7duY" width=500px height=500px></iframe></ui>
-			<br><br>			
+			<p><h4>Greenwich Village Comedy Club Showcase with Talia Reese/Steve Arons</p></h4>
 			<p><h4>Stand Up 360 Comedy Festival at Broadway Comedy Club (New York, NY)</h4> </p>
 			<img src="_images/standup-me.jpg" style="max-height: 400px; max-width: 400px;">
-			<p><h4>The Stand NYC Comedy Club (Laughing Buddha Industry Showcase) (New York, NY)</h4></p>
+			<p><h4>The Stand NYC Comedy Club (New York, NY)</h4></p>
 			<img src="_images/the-stand-nyc.png" style="max-height: 400px; max-width: 400px;">
 			<p><h4>Las Vegas World Series of Comedy Festival (Las Vegas, Nevada)</h4></p>
 
 			<p><h2>2023-Present</h2></p>
 
 			<p><h4>Flappers Comedy Club (Burbank, California)</h4></p>
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/HTC6hIr4_nI?start=4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-
 			<p><h4>2nd Annual Comedy Chateau International Comedy Festival - Finalist/Best of Fest (Los Angeles, California)</h4></p>
-			<img src="_images/chateau.png" style="max-height: 400px; max-width: 400px;">
-			<img src="_images/chateau-comedy.png" style="max-height: 400px; max-width: 400px;">
-
 			<p><h4>Portland Maine Comedy Festival (Portland, Maine)</h4></p>
 			<p><h4>North Carolina Comedy Festival (Greensboro, North Carolina)</h4></p>
+			<p><h4>Fourth Wall Comedy Cafe</h4></p>
+			<p><h4>The Hollywood Comedy</h4></p>
+			<p><h4>Comedy Blvd</h4></p>
+			<p><h4>The Ice House Comedy Club Pasadena</h4></p>
 			</div>
 
 			
