17f05bccbd96560497e41adccbafe3e44aa260dc
Terry Bu <terrybu88@gmail.com>
2024-05-12 13:09:55 -0700

    fixed links:

--- comedy.php
+++ comedy.php
@@ -36,25 +36,26 @@
 				<li>Comedy Chateau International Comedy Festival - Finalist/Best of Fest (Los Angeles, California) 2023</li>
 				<li>Portland Maine Comedy Festival (Portland, Maine) 2023 </li>
 				<li>North Carolina Comedy Festival (Greensboro, North Carolina) 2023 </li>
-				<li>Fourth Wall Comedy Cafe (Los Angeles, California)</li>
 				<li>The Hollywood Comedy (Los Angeles, California)</li>
 				<li>Comedy Blvd (Los Angeles, California)</li>
 				<li>The Ice House Comedy Club Pasadena</li>
 				<li>The Comedy Store (Los Angeles, California)</li>
 				<li>Boston Comedy Festival 2024 (Boston, Massachusetts)</li>
-				<ui> <iframe width="560" height="315" src="https://youtu.be/XAL2h7bf-68" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></ui>
+				<li>Fourth Wall Comedy Cafe (Los Angeles, California)</li>
+
+				<ui> <iframe width="560" height="315" src="https://www.youtube.com/embed/XAL2h7bf-68?si=-mn18D9_g7w1jwiw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></ui>
 			</ul>
 
 			<p><h2>2013-2016</h2></p>
 			<ul>
 			<li>Greenwich Village Comedy Club Showcase with Talia Reese/Steve Arons</li>
-			<ui> <iframe width="560" height="315" src="https://youtu.be/9g_1bIP7duY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></ui>
+			<ui> <iframe width="560" height="315" src="https://www.youtube.com/embed/9g_1bIP7duY?si=o0QQeoEWUtSpAWWj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></ui>
 
 			<li>Stand Up 360 Comedy Festival at Broadway Comedy Club (New York, NY) </li>
 			<img src="_images/standup-me.jpg" style="max-height: 400px; max-width: 400px;">
-			
+
 			<li>The Stand NYC Comedy Club (New York, NY)</li>
-			<ui> <iframe width="560" height="315" src="https://youtu.be/DXokKdpo4AU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></ui>
+			<ui><iframe width="560" height="315" src="https://www.youtube.com/embed/DXokKdpo4AU?si=MLjLRO8d7fo-6A_y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></ui>
 
 			<p><h2>2013</h2></p>
 			<ul>
