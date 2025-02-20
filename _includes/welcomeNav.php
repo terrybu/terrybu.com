83f98010f8ac2eeed6ecd8d6bfc283f5d7f18776
Terry Bu <terrybu88@gmail.com>
2025-02-20 13:20:21 -0800

    comment blog

--- _includes/welcomeNav.php
+++ _includes/welcomeNav.php
@@ -31,7 +31,7 @@
 	        <li><a href="https://github.com/terrybu" target="_blank">GitHub</a></li>
 	      </ul>
 	    </li> -->
-   	    <li><a href="https://www.terrybu.com/blog"><span class="glyphicon glyphicon-pencil" style="margin-right: 2px;"></span>BLOG</a></li>
+   	    <!-- <li><a href="https://www.terrybu.com/blog"><span class="glyphicon glyphicon-pencil" style="margin-right: 2px;"></span>BLOG</a></li> -->
 	    <!-- <li><a href="https://www.terrybu.com/resume.pdf"><span class="glyphicon glyphicon-paperclip" style="margin-right: 2px;"></span>RESUME</a></li> -->
 	    <li><a href="contact.php"><span class="glyphicon glyphicon-user" style="margin-right: 2px;"></span>CONTACT</a></li>
 	  </ul>
