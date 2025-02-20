4e53fa0d946e8677be4cb273fd27f7bcafc1f2a3
Terry Bu <terrybu88@gmail.com>
2025-02-20 13:46:06 -0800

    schedule

--- schedule.php
+++ schedule.php
@@ -22,11 +22,11 @@
         <div id="bodyContent">
           <h1 style="color: #C63B3B; clear: both;">Schedule</h1>
           <pre>
-            2/20 Flappers Yoohoo
+            2/20 Flappers Comedy Club and Restaurant (Yoohoo Room)
             3/1 Glendale Room
             3/7 Fourth wall cafe
-            3/14 Freaky Friday
-            3/16 Flashback
+            3/14 Freaky Friday (Glendale Room)
+            3/16 Flashback Studio 
             3/29 Tao Comedy Studio
           </pre>
             <a href="https://www.linktree.com/terrybu">https://www.linktree.com/terrybu</a>
