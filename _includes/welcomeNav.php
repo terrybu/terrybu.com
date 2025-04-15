3cca1323a08d6202745f6d214950f488e8cb6f6f
Terry Bu <terrybu88@gmail.com>
2025-04-15 12:39:10 -0700

    music tab update

--- _includes/welcomeNav.php
+++ _includes/welcomeNav.php
@@ -14,14 +14,6 @@
 	    <li><a href="comedy.php"><span class="glyphicon glyphicon-glass" style="margin-right: 2px;"></span>COMEDY</a></li>
 	   	<li><a href="books.php"><span class="glyphicon glyphicon-book" style="margin-right: 2px;"></span>BOOKS</a></li>
 
-<!-- 	    <li class="dropdown"><a href="#" id="action" data-toggle="dropdown" class="dropdown-toggle"><span class="glyphicon glyphicon-facetime-video	
-" style="margin-right: 5px;"></span>CONTENT<b class="caret"></b></a>
-	      <ul class="dropdown-menu">
-	        <li><a href="https://www.youtube.com/terrybu">YouTube</a></li> -->
-	        <!-- <li><a href="podcast.php">Podcast</a></li> -->
-	<!--       </ul>
-	    </li> -->
-
 <!-- 	    <li class="dropdown"><a href="#" id="action" data-toggle="dropdown" class="dropdown-toggle"><span class="glyphicon glyphicon-folder-open" style="margin-right: 5px;"></span>PORTFOLIO<b class="caret"></b></a>
 	      <ul class="dropdown-menu">
 	        <li><a href="client_work.php">Client Work</a></li>
@@ -33,7 +25,7 @@
 	    </li> -->
    	    <li><a href="https://terrybu88.blogspot.com/"><span class="glyphicon glyphicon-pencil" style="margin-right: 2px;"></span>BLOG</a></li>
 	    <!-- <li><a href="https://www.terrybu.com/resume.pdf"><span class="glyphicon glyphicon-paperclip" style="margin-right: 2px;"></span>RESUME</a></li> -->
-	    <li><a href="music.php"><span class="glyphicon glyphicon-music" style="margin-right: 2px;"></span>Music</a></li>
+	    <li><a href="music.php"><span class="glyphicon glyphicon-music" style="margin-right: 2px;"></span>MUSIC</a></li>
 	    <li><a href="contact.php"><span class="glyphicon glyphicon-user" style="margin-right: 2px;"></span>CONTACT</a></li>
 	  </ul>
 	</div>
