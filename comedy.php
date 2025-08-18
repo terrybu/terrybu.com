aee2466ca59c8b5264bcfd6a4e1cd4616aacbcc6
Terry Bu <terrybu88@gmail.com>
2025-08-18 11:20:56 -0700

    recent videos

--- comedy.php
+++ comedy.php
@@ -35,12 +35,13 @@
 			<h1>Credits & Club Appearances</h1>
 
 			<ul>
+				<li>Burbank Comedy Festival 2024,2025 - Best of Fest</li>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/g0Fxndh32Hg?si=PBKQUkfiEzSaCGen" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				<li>Three Clubs Lounge (Los Angeles) 2025</li>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/ZGMwG0HuzFA?si=Y0gYxObTRcLOKdFz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				<li>Westside Comedy Club (Santa Monica, Los Angeles) 2025</li>
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/OYZR9pExN8Q?si=6-QHTPeyZ2-hb9bX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				<li>Flappers Comedy Club (Burbank, California)</li>
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/UTErwsN07fc?si=LSLTqO0-TJjmWiQL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				<li>Fourth Wall Comedy Cafe (Los Angeles, California)</li>
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/3h05HL5drac?si=c4WGQQaEdFlApe8m" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				<li>Tao Comedy Studio 2025</li>
@@ -50,7 +51,6 @@
 				<li>Boston Comedy Festival 2024 (Boston, Massachusetts)</li>
 				<li>Portland Maine Comedy Festival (Portland, Maine) 2023 </li>
 				<li>North Carolina Comedy Festival (Greensboro, North Carolina) 2023, 2025 </li>
-				<li>Burbank Comedy Festival 2024,2025 - Best of Fest</li>
 				<iframe width="560" height="315" src="https://www.youtube.com/embed/QdgBpc5DKUw?si=axnXXoL4eU9FrokP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				<li>Comedy Chateau International Comedy Festival - Finalist/Best of Fest (Los Angeles, California) 2023</li>
 				<li>Flashback Studios (Low Hanging Fruit show)</li>
