ab1e3e27654482ddfd2ced441bc2f38359083421
Terry Bu <terrybu@Terry-Learnosity-Macbook-2026.local>
2026-08-11 14:41:28 -0600

    icons fix

--- _includes/gotSocial.php
+++ _includes/gotSocial.php
@@ -2,7 +2,7 @@
 <span>got Social?</span>
 <a href="https://www.instagram.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_instagram.png" alt="terry instagram"></a>
 
-<a href="https://www.tiktok.com/@terrybu88" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_tiktok.jpg" alt="tiktok"></a>
+<a href="https://www.tiktok.com/@terrybu88" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_tiktok.png" alt="tiktok"></a>
 
 <a href="https://www.youtube.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_youtube.png" alt="terry youtube"></a>
 <a href="https://open.spotify.com/show/41MKPWhKzka8xw5h7oV7ld" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_spotify.png" alt="terry podcast"></a>
