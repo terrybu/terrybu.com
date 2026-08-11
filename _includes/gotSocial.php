c54573d242f8835d740ffce9b7dd855f1c14c224
Terry Bu <terrybu@Terry-Learnosity-Macbook-2026.local>
2026-08-11 14:44:06 -0600

    fix size

--- _includes/gotSocial.php
+++ _includes/gotSocial.php
@@ -2,12 +2,12 @@
 <span>got Social?</span>
 <a href="https://www.instagram.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_instagram.png" alt="terry instagram"></a>
 
-<a href="https://www.tiktok.com/@terrybu88" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_tiktok.png" alt="tiktok"></a>
+<a href="https://www.tiktok.com/@terrybu88" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_tiktok.png" alt="tiktok" width="45" height="45"></a>
 
 <a href="https://www.youtube.com/terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_youtube.png" alt="terry youtube"></a>
 <a href="https://open.spotify.com/show/41MKPWhKzka8xw5h7oV7ld" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_spotify.png" alt="terry podcast"></a>
 
-<a href="https://www.threads.com/@terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_threads.png" alt="Threads"></a>
+<a href="https://www.threads.com/@terrybu" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_threads.png" alt="Threads" width="45" height="45"></a>
 <a href="https://open.spotify.com/show/41MKPWhKzka8xw5h7oV7ld" target="_blank" onmouseover="document.getElementById('1up').play()"><img src="_images/icon_spotify.png" alt="terry podcast"></a>
 
 <!-- <a href="https://www.linkedin.com/in/terrybu" target="_blank"><img src="_images/icon_linkedin_new.png" alt="terry linkedin" onmouseover="document.getElementById('1up').play()"></a> -->
